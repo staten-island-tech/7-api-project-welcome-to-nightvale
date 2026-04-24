7aba382ebb134d39a62cabbac1e0466d3b3dadc8
screaming in agony <ameliar51@nycstudents.net>
2026-04-24 10:16:43 -0400

    snickering

--- french_toast.py
+++ french_toast.py
@@ -1,9 +1,14 @@
 import requests
+import tkinter as tk
+
 def Trees(tree):
-    response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json{tree}")
+    response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json?tree_id={tree}")
     data = response.json()
     return data
 
 tree = Trees("199062")
-print(tree)
+""" print(tree[0]["spc_common"]) """
 
+window = tk.Tk()
+window.title("tree !! !!! !")
+w
