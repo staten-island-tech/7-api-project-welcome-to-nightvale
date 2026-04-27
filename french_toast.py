c6c135e9f4ad73f80db35d6d11f3edd3ed247579
screaming in agony <ameliar51@nycstudents.net>
2026-04-27 10:17:19 -0400

    heheheh

--- french_toast.py
+++ french_toast.py
@@ -7,8 +7,28 @@ def Trees(tree):
     return data
 
 tree = Trees("199062")
-""" print(tree[0]["spc_common"]) """
+print(tree[0]["spc_common"])
 
 window = tk.Tk()
 window.title("tree !! !!! !")
-w
+window.geometry("400x250")
+label = tk.Label(window, text="staring ominously")
+font=("Comic Sans MS", 12)
+label.pack(pady=10)
+button = tk.Button(window, text="evil button", width=15, command=window.destroy)
+font=("Comic Sans MS", 12)
+button.pack()
+entry=tk.Entry(window, font=("Comic Sans MS", 12), width=30)
+entry.grid(row=0, column=1)
+entry.pack(pady=5)
+
+window.mainloop()
+
+
+""" def divide(a, b):
+    try:
+        result = a/b
+    except ZeroDivisionError:
+        print("cant divide by 0")
+    else:
+        print(result) """
