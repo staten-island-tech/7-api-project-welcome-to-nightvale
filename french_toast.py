66eb56dd70ff8e88dd22fb238bd33b2744970e29
screaming in agony <ameliar51@nycstudents.net>
2026-04-28 10:12:23 -0400

    scheming

--- french_toast.py
+++ french_toast.py
@@ -1,26 +1,30 @@
 import requests
 import tkinter as tk
 
-def Trees(tree):
+""" def Trees(tree):
     response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json?tree_id={tree}")
     data = response.json()
     return data
 
 tree = Trees("199062")
-print(tree[0]["spc_common"])
+print(tree[0]["spc_common"]) """
 
 window = tk.Tk()
 window.title("tree !! !!! !")
-window.geometry("400x250")
-label = tk.Label(window, text="staring ominously")
-font=("Comic Sans MS", 12)
+window.geometry("600x450")
+data_type = tk.StringVar()
+inp = tk.StringVar()
+label = tk.Label(window, font=("Comic Sans MS", 10), text="enter the data you would like to access:\n\nTree ID, Block ID, Status, Scientific name, Common name, Zip code")
 label.pack(pady=10)
-button = tk.Button(window, text="evil button", width=15, command=window.destroy)
-font=("Comic Sans MS", 12)
-button.pack()
-entry=tk.Entry(window, font=("Comic Sans MS", 12), width=30)
-entry.grid(row=0, column=1)
+entry=tk.Entry(window, textvariable = data_type, font=("Comic Sans MS", 10), width=30)
 entry.pack(pady=5)
+
+def search():
+    dtype = data_type.get()
+    print(f"Searched for {dtype}")
+
+button = tk.Button(window, font=("Comic Sans MS", 10), text="search", width=15, command=search)
+button.pack(pady=5)
 
 window.mainloop()
 
