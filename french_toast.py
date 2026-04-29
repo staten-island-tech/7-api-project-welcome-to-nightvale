dbe3389664d46ad45ab90b0ddb874c1bef3722fe
screaming in agony <ameliar51@nycstudents.net>
2026-04-29 10:16:59 -0400

    :3

--- french_toast.py
+++ french_toast.py
@@ -1,10 +1,10 @@
 import requests
 import tkinter as tk
+url = "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
 
 """ def Trees(tree):
     response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json?tree_id={tree}")
     data = response.json()
-    return data
 
 tree = Trees("199062")
 print(tree[0]["spc_common"]) """
@@ -13,6 +13,7 @@ window = tk.Tk()
 window.title("tree !! !!! !")
 window.geometry("600x450")
 data_type = tk.StringVar()
+value = tk.StringVar()
 inp = tk.StringVar()
 label = tk.Label(window, font=("Comic Sans MS", 10), text="enter the data you would like to access:\n\nTree ID, Block ID, Status, Scientific name, Common name, Zip code")
 label.pack(pady=10)
@@ -20,10 +21,14 @@ entry=tk.Entry(window, textvariable = data_type, font=("Comic Sans MS", 10), wid
 entry.pack(pady=5)
 
 def search():
-    dtype = data_type.get()
-    print(f"Searched for {dtype}")
+    print("hi")
 
-button = tk.Button(window, font=("Comic Sans MS", 10), text="search", width=15, command=search)
+def next():
+    dtype = data_type.get()
+    label.config(text=f"enter value for {dtype}", textvariable = value)
+    button.config(text="search", command=search, command=button.destroy())
+
+button = tk.Button(window, font=("Comic Sans MS", 10), text="continue", width=15, command=next)
 button.pack(pady=5)
 
 window.mainloop()
