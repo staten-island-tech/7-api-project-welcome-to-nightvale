f2691da25078353acc38115b064b9eed76f618d0
screaming in agony <ameliar51@nycstudents.net>
2026-05-01 09:55:26 -0400

    explode

--- french_toast.py
+++ french_toast.py
@@ -1,12 +1,9 @@
 import requests
 import tkinter as tk
 url = "https://data.cityofnewyork.us/resource/uvpi-gqnh.json"
+#cname = url + "?=spc_common"
 
-""" def Trees(tree):
-    response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json?tree_id={tree}")
-    data = response.json()
-
-tree = Trees("199062")
+""" tree = Trees("180683")
 print(tree[0]["spc_common"]) """
 
 window = tk.Tk()
@@ -15,18 +12,31 @@ window.geometry("600x450")
 
 value = tk.StringVar()
 
-label = tk.Label(window, font=("Comic Sans MS", 10), text="enter tree id")
+label = tk.Label(window, font=("Comic Sans MS", 10), text="enter six digit tree id")
 label.pack(pady=10)
 val_entry=tk.Entry(window, textvariable = value, font=("Comic Sans MS", 10), width=10)
 val_entry.pack(pady=5)
 
-def search():
-    treeid = value.get()
-    label.config(text=f"Tree #{treeid}\nCommon Name: {url + "?=spc_common"}", anchor="w", justify=tk.LEFT)
-    val_entry.destroy()
-    button.destroy()
+def trees():
+    tree = value.get()
+    response = requests.get(f"https://data.cityofnewyork.us/resource/uvpi-gqnh.json?tree_id={tree}")
+    data = response.json()
+    try:
+        print(data[0]["tree_id"])
+    except IndexError:
+        label.configure(text="Tree not found. \n\n Please enter a valid six digit number")
+    else:
+        label.configure(text = f"Tree ID= {tree} \n\nCommon name: {data[0]["spc_common"]} \nScientific name: {data[0]["spc_latin"]} \nAddress: {data[0]["address"]}", anchor="w", justify=tk.LEFT)
+        val_entry.destroy()
+        button.destroy()
 
-button = tk.Button(window, font=("Comic Sans MS", 10), text="search", width=15, command=search)
+""" def search():
+    treeid = value.get()
+    label = tk.Label(command = Trees, anchor="w", justify=tk.LEFT)
+    val_entry.destroy()
+    button.destroy() """
+
+button = tk.Button(window, font=("Comic Sans MS", 10), text="search", width=15, command=trees)
 button.pack(pady=5)
 
 window.mainloop()
